6796be223a165b852cbffb403982f3f66bd5d07e
Faiaz Hossain <faiazhossain10@gmail.com>
2022-11-09 15:27:28 +0600

    added mongoDb and .env

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
-const port = process.env.PORT || 5000; // middleware
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+
+// middleware
 app.use(cors());
 app.use(express.json());
+
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASSWORD);
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fpsp1lt.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 app.get("/", (req, res) => {
   res.send("Ilmas dentistry server is running");
