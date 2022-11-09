202594cbc1828f089224847a604aa0f4e21a9294
Faiaz Hossain <faiazhossain10@gmail.com>
2022-11-09 13:41:58 +0600

    server running

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-
-// middleware
+const port = process.env.PORT || 5000; // middleware
 app.use(cors());
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Ilmas dentistry server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Ilmas dentistry server is running of port ${port}`);
+});
