00dd5b75e67904b4075353001db0f866f8b84715
Faiaz Hossain <faiazhossain10@gmail.com>
2022-11-10 12:53:57 +0600

    added query

--- index.js
+++ index.js
@@ -51,7 +51,12 @@ async function run() {
 
     // get Reviews
     app.get("/reviews", async (req, res) => {
-      const query = {};
+      let query = {};
+      if (req.query.service) {
+        query = {
+          service: req.query.service,
+        };
+      }
       const cursor = reviewCollection.find(query);
       const reviews = await cursor.toArray();
       res.send(reviews);
