af0d2736656d07214499582f0eccc3e629d36a58
Faiaz Hossain <faiazhossain10@gmail.com>
2022-11-10 06:23:47 +0600

    limit services added

--- index.js
+++ index.js
@@ -24,14 +24,20 @@ async function run() {
     const serviceCollection = client
       .db("ilmasDentistry")
       .collection("services");
-    app.get("/services", async (req, res) => {
+    app.get("/allServices", async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
       const services = await cursor.toArray();
       res.send(services);
     });
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query).limit(3);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
 
-    app.get("/services/:id", async (req, res) => {
+    app.get("/allServices/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const service = await serviceCollection.findOne(query);
